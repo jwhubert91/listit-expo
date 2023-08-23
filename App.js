d8cf049d374108f92055c147dfff6d5b29167d9c
James Hubert <jwhubert91@gmail.com>
2023-08-22 21:04:01 -0400

    comments

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from "expo-status-bar"
 import { StyleSheet, Text, View } from "react-native"
 
+// left off here: https://docs.expo.dev/develop/user-interface/splash-screen/
+
 export default function App() {
   return (
     <View style={styles.container}>
